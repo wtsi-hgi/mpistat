2ace8b6db8ec35bc15d02b3c3c2cd42159ffe54d
Emyr James <emyrjames1@gmail.com>
2015-01-14 18:31:47 +0000

    leaving the gzip till the end. postgresql's copy-from just won't work nicely when the client and server are on different machines

--- bin/mpistat_tidy.sh
+++ bin/mpistat_tidy.sh
@@ -11,22 +11,15 @@ DATE=`date +%Y%m%d`
 
 # cat the scratch data files to a gzipped output file with a name based on the
 # current date
-DATA_FILE=$BASE/data/${DATE}_${VOL}.dat.gz
+DATA_FILE=$BASE/data/${DATE}_${VOL}.dat
 if [[ -e ${DATA_FILE} ]]
 then
     echo "Data file ${DATA_FILE} already exists, refusing to clobber."
 else
-    cat $BASE/data/$VOL/* | gzip > ${DATA_FILE}
+    cat $BASE/data/$VOL/* > ${DATA_FILE}
+    rm $BASE/data/$VOL/*
 fi
 
 # TO DO:
 # run a report generator against it
-
-# ensure a fifo for the psql copy exists
-touch $BASE/psql_pipe
-rm $BASE/psql_pipe
-mkfifo $BASE/psql_pipe
-
-# and dump the data into the daily postgresql db
-
 
