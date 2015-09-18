38e3533cbf0c6e787dbc750d32ed7ede69025307
Emyr James <emyrjames1@gmail.com>
2015-09-18 15:32:59 +0100

    First iteration of the c version. It seems to work and outputs the right data but it doesn't seem to finish cleanly enough.

--- src/mpistat.c
+++ src/mpistat.c
@@ -6,12 +6,12 @@
 #include <errno.h>
 #include <dirent.h>
 #include <limits.h>
-
+#include <sys/stat.h>
 // globals
 // dirty... but KISS for now...
 FILE *out; // rank specific output file handle
-char start_dir[4096]; // absolute path of start directory
-char item_buf[5000]; // buffer to construct type / path combos for queue items
+char start_dir[8192]; // absolute path of start directory
+char item_buf[8192]; // buffer to construct type / path combos for queue items
 
 char file_type(unsigned char c) {
     switch (c) {
@@ -34,6 +34,44 @@ char file_type(unsigned char c) {
     }
 }
 
+void do_lstat(char *path) {
+    static struct stat buf;
+    if (lstat(path+1,&buf) == 0) {
+        fprintf(out,"%s\t%c\t%d\t%d\t%d\t%d\t%d\t%d\n", path+1, *path,
+                buf.st_size, buf.st_uid, buf.st_gid, buf.st_atime,
+                buf.st_mtime, buf.st_ctime);
+    } else {
+        fprintf (stderr, "Cannot lstat '%s': %s\n", path+1, strerror (errno));
+    }
+
+}
+
+void do_readdir(char *path, CIRCLE_handle *handle) {
+    int path_len=strlen(path+1);
+    DIR *d = opendir (path+1);
+    if (!d) {
+        fprintf (stderr, "Cannot open '%s': %s\n", path+1, strerror (errno));
+        return;
+    }
+    while (1) {
+        struct dirent *entry;
+        entry = readdir(d);
+        if (entry==0) {
+            break;
+        }
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") ==0) {
+            continue;
+        }
+        char *tmp=(char*) malloc(path_len+strlen(entry->d_name)+3);
+        *tmp=file_type(entry->d_type);
+        strcpy(tmp+1,path+1);
+        *(tmp+path_len+1)='/';
+        strcpy(tmp+path_len+2,entry->d_name);
+        handle->enqueue(tmp);
+    }
+    closedir(d);
+}
+
 // create work callback
 // this is called once at the start on rank 0
 // use to seed rank 0 with the initial dir to start
@@ -46,16 +84,11 @@ void create_work(CIRCLE_handle *handle) {
 void process_work(CIRCLE_handle *handle)
 {
     // dequeue the next item
-
-    // skip . and ..
-
-    // lstat it and dump the lstat data
-    // for now, just print the filename to the
-    // output file
-
-    // if it's a directory call readdir on it
-    // and add all the items in the directory to the work queue
-
+    handle->dequeue(item_buf);
+    do_lstat(item_buf);
+    if (*item_buf == 'd') {
+        do_readdir(item_buf,handle);
+    }
 }
 
 // arguments :
@@ -72,7 +105,7 @@ int main(int argc, char **argv) {
     // final output file
     // the resulting string will be used to seed the queue in the
     // create work callback
-    if (argc != 2) {
+    if (argc != 3) {
         fprintf(stderr, "Usage : mpistat <data dir> <start dir>\n");
         return 1;
     }
@@ -80,7 +113,7 @@ int main(int argc, char **argv) {
     DIR *sd=opendir(start_dir);
     if (!sd) {
         fprintf (stderr, "Cannot open directory '%s': %s\n",
-            argv[1], strerror (errno));
+            start_dir, strerror (errno));
         exit (EXIT_FAILURE);
     }
     sprintf(item_buf,"%c%s",'d',start_dir);
@@ -91,7 +124,7 @@ int main(int argc, char **argv) {
 	// open output file for this particular rank
     // (colllated at the end)
 	char *file_name=(char*) malloc(200);;
-	sprintf(file_name,"%s/%02d.out",argv[1],rank);
+	sprintf(file_name,"%s/%02d.out",argv[2],rank);
 	out=fopen(file_name, "w");
 
     // set the create work callback
